e6cfdbc2f308c021bbf0d0b3b2272b68dc5200e4
GRE3DISGOOD <gaetanhonthaas@gmail.com>
2023-04-04 16:56:04 +0200

    redeploy backup cause of error

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once 'cors.php';
+// Set the response headers to allow cross-origin requests
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
 
 try {
     // Connect to the database
