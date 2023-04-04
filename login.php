5ed288d7025d3452e4623e17952b6ea23ba2dd4e
GRE3DISGOOD <gaetanhonthaas@gmail.com>
2023-04-04 16:36:08 +0200

    maj hearders

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 
 // Set the response headers to allow cross-origin requests
-header('Access-Control-Allow-Origin: https://gaetan-hts.github.io');
-header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 
